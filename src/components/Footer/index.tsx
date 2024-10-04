aaaeea8ea7e779cc690c81e32da13121cc9a1c7e
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2024-10-04 10:38:42 -0500

    Add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,51 @@
+import styles from './styles.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        {/* <!-- About Column --> */}
+        <div className={styles.footer__column}>
+          <h3>About Me</h3>
+          <p>
+            I&apos;m Rafael Martinez, an Electronics Engineer and Programmer passionate about technology and
+            innovation.
+          </p>
+        </div>
+
+        {/* <!-- Links Column --> */}
+        <div className={styles.footer__column}>
+          <h3>Quick Links</h3>
+          <ul>
+            <li>
+              <a href='#about'>About</a>
+            </li>
+            <li>
+              <a href='#projects'>Projects</a>
+            </li>
+            <li>
+              <a href='#contact'>Contact</a>
+            </li>
+          </ul>
+        </div>
+
+        {/* <!-- Social Media Column --> */}
+        <div className={styles.footer__column}>
+          <h3>Follow Me</h3>
+          <div className={styles.social__icons}>
+            <a href='https://github.com/rafaelmber' target='_blank'>
+              GitHub
+            </a>
+            <a href='https://www.linkedin.com/in/rafaelmber' target='_blank'>
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.footer__bottom}>
+        <p>&copy; 2024 Rafael Martinez. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
