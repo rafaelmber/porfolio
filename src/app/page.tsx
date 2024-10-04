e3a35789628a380518fc17414cbcf470ec3c0d20
Rafael Martinez Bermudez <rafaelmartinez2699@gmail.com>
2024-10-04 11:14:10 -0500

    Create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import Image from 'next/image';
+
+import ProductCard from '@/components/ProjectCard';
+import Wrapper from '@/components/Layout/Wrapper';
+
+import profilePic from '@/public/images/profile.jpg';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <Wrapper>
+      <section className={styles.hero_section}>
+        <h1>Welcome to My Portfolio</h1>
+        <p>I&apos;m Rafael Martinez, an Electronics Engineer and Programmer</p>
+        <a href='#projects'>Explore My Work</a>
+      </section>
+      <section id='about' className={styles.section_card}>
+        <h2>About Me</h2>
+        <div className={styles.about}>
+          <div className={styles.image_container}>
+            <Image className={styles.profile_picture} src={profilePic} width={240} height={300} alt='Picture' />
+          </div>
+          <div className={styles.content}>
+            <p>
+              I&apos;m <strong>Rafael Martinez</strong>, an Electronics Engineer and Developer passionate about
+              solving real-world problems through technology. I specialize in electronics, microcontroller
+              programming, and software development, with experience in frontend development and integrating
+              hardware with code.
+            </p>
+            <p>
+              My goal is to create impactful solutions that merge hardware and software, while continuously growing
+              as a developer and engineer. Whether it&apos;s a new app or a microcontroller-based project, I love
+              pushing the boundaries of innovation.
+            </p>
+            <button className={styles.primary_button}>Contact Me</button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className={styles.section_card}>
+        <h2>Skills</h2>
+        <div className={styles.skills_container}>
+          <h3>Technologies & Expertise</h3>
+          <ul className={styles.skills_list}>
+            <li>React</li>
+            <li>Python</li>
+            <li>Microcontroller Programming</li>
+            <li>Data Science</li>
+            <li>Frontend Development</li>
+            <li>Electronics</li>
+          </ul>
+        </div>
+      </section>
+      <section id='projects' className={styles.section_card}>
+        <h2>Projects</h2>
+        <div className={styles.projects}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+        <button className={styles.primary_button}>More Projects</button>
+      </section>
+    </Wrapper>
   );
 }
